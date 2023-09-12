64415b162268cc6e5bb72b3e4d0cdaa7b01b841c
Swetha Kakumanu <swethakakumanu20@gmail.com>
2023-09-12 15:24:55 +0530

    Add files via upload

---
+++ Day4/EntityModelClasses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day4_Assignment.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; } 
+        public string ProductName { get; set; }
+        public double UnitPrice {  get; set; }
+        public int Quantity { get; set; }
+    }
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set;}
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
---
+++ Day4/ProductApiController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Day4_Assignment.Models;
+namespace Day4_Assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private ProductDbContext _dbConext;
+
+        public ProductApiController(ProductDbContext dbContext)
+        {
+            _dbConext = dbContext;
+        }
+
+        // Read All
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_dbConext.Products.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Product obj)
+        {
+            _dbConext.Products.Add(obj);
+            _dbConext.SaveChanges();
+            return Ok(new { result = "Emp Details added to db" });
+        }
+
+        [HttpPut]
+        public IActionResult Edit([FromBody] Product obj)
+        {
+            _dbConext.Entry(obj).State = EntityState.Modified;
+            _dbConext.SaveChanges();
+            return Ok(new { result = "Emp Details updated to db" });
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            Product obj = _dbConext.Products.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound(new { result = "Requested employee details are not available." });
+            }
+            else
+            {
+                return Ok(obj);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Product obj = _dbConext.Products.Find(id);
+            _dbConext.Products.Remove(obj);
+            _dbConext.SaveChanges();
+            return Ok(new { result = "Emp Details deleted from db" });
+        }
+    }
+}
