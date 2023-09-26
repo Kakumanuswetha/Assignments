bef85ec7f15ce5e37601fbfa0bd9b1b6d5f58574
Swetha Kakumanu <swethakakumanu20@gmail.com>
2023-09-26 13:31:10 +0530

    Add files via upload

---
+++ WebApiCaseStudy/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using WebApiCaseStudy.Models;
+namespace WebApiCaseStudy.Repositories
+{
+    public interface IProductRepository
+    {
+        public void Create(Product obj);
+        public void Edit(int id);
+        public List<Product> Details();
+        public Product DetailsById(int id);
+        public void Delete(int id);
+    }
+}
